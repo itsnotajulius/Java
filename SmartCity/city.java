d6238a9c9cf6d8045bdaea5e6606af76b28a4dd3
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-12 20:11:20 +0200

    Update SmartCity/city.java

--- SmartCity/city.java
+++ SmartCity/city.java
@@ -1,4 +1,5 @@
 // This App is used to convert from on Currency to another
 /*
-    Reference for the project is from
+    Reference for the project are:
+    - https://hackr.io/blog/java-projects but making it my own.
 */
