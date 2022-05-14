14b1d247dde4dbe2f0db7dc519e9fbdaaad83a51
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-15 01:22:22 +0200

    Update SmallProjects/DecimalComparator.java

---
+++ SmallProjects/DecimalComparator.java
@@ -0,0 +1,10 @@
+public class DecimalComparator{
+    public static boolean areEqualByThreeDecimalPlaces(double num1,double num2){
+           boolean answer = false;
+           if((int)(num1*1000)==(int)(num2*1000)){
+               answer = true;
+           } 
+           return answer;
+           
+       }
+   }
