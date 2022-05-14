b58807f73f47a5b000d54f7602c02c93527428d1
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-15 00:29:41 +0200

    Update SmallProjects/SpeedConverter.java

---
+++ SmallProjects/SpeedConverter.java
@@ -0,0 +1,27 @@
+public class SpeedConverter {
+
+    public static long toMilesPerHour(double kilometersPerHour){
+        long answer = -1L;
+        if(kilometersPerHour>=0){
+            answer =  Math.round(kilometersPerHour/1.609);
+        }
+        return answer;
+        
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        if(kilometersPerHour>=0){
+            System.out.println(kilometersPerHour+" km/h = "+Math.round(kilometersPerHour/1.609)+" mi/h");
+        }else{
+            System.out.println("Invalid Value");
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        printConversion(1.5);
+        printConversion(10.25);
+        printConversion(-5.6);
+        printConversion(25.42);
+        printConversion(75.114);
+    }
+}
