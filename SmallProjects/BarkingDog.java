3b6d39d047b8d707f43c4a11a37fc6a6a0d211b9
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-15 00:55:35 +0200

    Update SmallProjects/BarkingDog.java

---
+++ SmallProjects/BarkingDog.java
@@ -0,0 +1,10 @@
+public class BarkingDog {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay){
+       boolean answer = false;
+       if(barking&&(hourOfDay<8||hourOfDay>22)&&(hourOfDay>=0&&hourOfDay<=23)){
+           answer = true;
+       }
+       return answer;
+
+   }
+}
