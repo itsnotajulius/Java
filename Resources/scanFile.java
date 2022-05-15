ba6eba99154e68f26d45f0004171ced200374ca4
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-15 03:19:47 +0200

    Update Resources/scanFile.java

--- Resources/scanFile.java
+++ Resources/scanFile.java
@@ -5,33 +5,28 @@ import java.util.*;
 public class scanFile {
 
     public static void main(String[] args) throws IOException {
-    // TODO code application logic here
-
-    // // read KeyWestTemp.txt
-
-    // create token1
+        // create token1
     String token1 = "";
     final String FILE_NAME =   "C:/Users/ItsNotAJulius/Documents/Coding/Java/Currencies/src/currencies.txt";
 
-    // for-each loop for calculating heat index of May - October
+    
 
-    // create Scanner inFile1
-    Scanner inFile1 = new Scanner(new File(FILE_NAME)).useDelimiter(";\\s*");
+    // create Scanner sc
+    Scanner sc = new Scanner(new File(FILE_NAME)).useDelimiter(";\\s*");
 
-    // Original answer used LinkedList, but probably preferable to use ArrayList in most cases
-    // List<String> temps = new LinkedList<String>();
+    
     List<String> temps = new ArrayList<String>();
 
     // while loop
-    while (inFile1.hasNextLine()) {
+    while (sc.hasNextLine()) {
       // find next line
-      while (inFile1.hasNext()) {
-        token1 = inFile1.next();
+      while (sc.hasNext()) {
+        token1 = sc.next();
         temps.add(token1);
       }
       
     }
-    inFile1.close();
+    sc.close();
 
     String[] tempsArray = temps.toArray(new String[0]);
 
