da90c56e670b5190df655dbdf7b4ebef600ef80b
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-24 10:15:29 +0200

    Update SmallProjects/Inheritance/Animal/main.java

---
+++ SmallProjects/Inheritance/Animal/main.java
@@ -0,0 +1,16 @@
+public class main{
+
+
+    public static void main(String[] args) {
+        for(int i = 0; i<10 ; i++){
+            System.out.println(i);
+        }
+    
+        
+    }
+
+}
+
+for(int i = 0; i<10 ; i++){
+    System.out.println(i);
+}
