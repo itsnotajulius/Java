378d71326dfc99459fbbf1ded9e1a9baa2c59795
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-25 22:39:06 +0200

    Add files via upload

---
+++ SmallProjects/OOP/Encapsulation/Player/App.java
@@ -0,0 +1,8 @@
+package src;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        EnhancedPlayer player = new EnhancedPlayer("Julius",200, "Batarang");
+        System.out.println("Health is "+player.remainHealth());
+    }
+}
---
+++ SmallProjects/OOP/Encapsulation/Player/Player.java
@@ -0,0 +1,17 @@
+package src;
+
+public class Player {
+    public String name;
+    public int health;
+
+    public void loseHealth(int damage){
+        this.health -= damage;
+        if(this.health<=0){
+            System.out.println("Player is died");
+        }
+    }
+
+    public int remainHealth(){
+        return this.health;
+    }
+}
