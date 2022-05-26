b6366d1ab801c1994065e6eba043fac4eb58dcfe
Matthew 'ItsNotaJulius' Julies <104577250+itsnotajulius@users.noreply.github.com>
2022-05-26 22:34:15 +0200

    Deleting SmallProjects/OOP/Interfaces

--- SmallProjects/OOP/Interfaces /ITelephone.java
+++
@@ -1,9 +0,0 @@
-package src.Interfacess;
-
-public interface ITelephone {
-    public void powerON();
-    public void dial(int phoneNumber);
-    public void answer();
-    public boolean callPhone(int phoneNumber);
-    public boolean isRinging();
-}
--- SmallProjects/OOP/Interfaces/Saving/ISaveable.java
+++
@@ -1,7 +0,0 @@
-package src.Interfacess.Saving;
-import java.util.List;
-
-public interface ISaveable {
-    public void read(List<String> savedValues);
-    public List<String> write();
-}
